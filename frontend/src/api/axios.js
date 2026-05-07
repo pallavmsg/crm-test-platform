45391367950c5b2b56b9a94954df15a63355ab0b
pallavmsg <pallav.choudhary@workspace.msgclub.co.in>
2026-05-07 17:36:02 +0530

    update frontend api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api/v1',
+    baseURL: 'https://crm-test-platform-production.up.railway.app/api/v1',
 });
 
 api.interceptors.request.use((config) => {
